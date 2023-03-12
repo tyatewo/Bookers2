97d3b9460117cc5753ef1249a6deeab539bcc3ec
Saori Sakai <124588744+tyatewo@users.noreply.github.com>
2023-03-12 11:25:37 +0000

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  #get 'homes/top'
-  devise_for :users
   root to: 'homes#top'
+  devise_for :users
+  #get 'users/show'
+  #get 'users/edit'
+  #get 'books/new'
+  #get 'books/index'
+  #get 'books/show'
+  #get 'books/edit'
+  #get 'homes/top'
+
+  resources :books, onry: [:index, :show, :edit]
+  resources :users, only: [:show, :edit, :update]
+
 end
