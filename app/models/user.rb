5c6805c374407078e2f4cd1f042321575f29d838
Saori Sakai <124588744+tyatewo@users.noreply.github.com>
2023-06-03 10:55:51 +0000

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,14 @@ class User < ApplicationRecord
   validates :introduction, length: { maximum: 50 }
 
 
+  def self.guest
+    find_or_create_by!(name: 'guestuser' ,email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "guestuser"
+    end
+  end
+
+
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get "/home/about" => "homes#about", as: "about"
   devise_for :users
 
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+
 
   resources :books, only: [:create, :index, :show, :edit, :destroy, :update]
   resources :users, only: [:show, :edit, :update, :index]
